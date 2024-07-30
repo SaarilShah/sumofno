4327ae92cf31d4302cf161dfc9453cc6fef78976
Saaril <saaril1155shah@gmail.com>
2024-07-30 23:57:49 +0530

    Succesful

---
+++ sumofno.java
@@ -0,0 +1,22 @@
+package sumofno;
+import java.util.Scanner;
+
+public class sumofno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int largest = Math.max(num1, Math.max(num2, num3));
+        System.out.println("The largest number is: " + largest);
+
+        scanner.close();
+    }
+}
